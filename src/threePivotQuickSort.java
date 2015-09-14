58fba22510c123b466ac6d1f56df8fbe3252a603
sean-stockholm <stock424@morris.umn.edu>
2015-09-13 19:36:20 -0500

    created the first two modifications of quicksort

---
+++ src/threePivotQuickSort.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+
+public class threePivotQuickSort {
+
+	public static int randomInRange(int lower, int upper) {
+		// if (lower == upper) return lower;
+		Random rand = new Random();
+		return lower + rand.nextInt(upper - lower + 1);
+	}
+
+	static long comparisonCount = 0;
+
+	public static <T extends Comparable<T>> void quickSort(T[] arr) {
+		if (arr.length > 1) {
+			int p = randomInRange(0, arr.length - 1);
+			int q = randomInRange(0, arr.length - 1);
+			int r = randomInRange(0, arr.length - 1);
+			T a;
+			int pq = arr[p].compareTo(arr[q]);
+			int pr = arr[p].compareTo(arr[r]);
+			int qr = arr[q].compareTo(arr[r]);
+			if (pq + pr == 0) swap(arr, p, arr.length-1);
+			if (pq + -qr == 0) swap(arr, q, arr.length-1);
+			if (pr + qr == 0) swap(arr, r, arr.length-1);
+			
+			quickSortRecursive(arr, 0, arr.length - 1);
+		}
+	}
+
+	private static <T extends Comparable<T>> void quickSortRecursive(T[] arr,
+			int left, int right) {
+		if (right - left > 0) {
+			// Step 1: set up swap pointer index
+
+			int swapIndex = left;
+			T pivotValue = arr[left];
+
+			// Step 2: swap pivot with the last value in the subarray */
+			swap(arr, left, right);
+
+			// Step 3: partition (hint: you'll need a loop)
+			int currentIndex = left;
+			while (currentIndex < right) {
+				comparisonCount++;
+				if (arr[currentIndex].compareTo(pivotValue) <= 0) {
+					swap(arr, swapIndex, currentIndex);
+					currentIndex++;
+					swapIndex++;
+				} else {
+					currentIndex++;
+				}
+			}
+
+			swap(arr, swapIndex, right);
+
+			// Step 4: Make the recursive calls
+			quickSortRecursive(arr, left, swapIndex - 1);
+			quickSortRecursive(arr, swapIndex + 1, right);
+		}
+	}
+
+	public static <T> void swap(T[] arr, int index1, int index2) {
+		T temp = arr[index2];
+		arr[index2] = arr[index1];
+		arr[index1] = temp;
+	}
+
+}
